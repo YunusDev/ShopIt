c8a3df54b9100ad9abdab569312fb053c39afc51
Yunus AbdulQudus <yunusabdulqudus1@gmail.com>
2019-06-15 22:57:35 +0100

    Second Commit....

--- database/seeds/UserSeederTable.php
+++ database/seeds/UserSeederTable.php
@@ -14,7 +14,7 @@ class UserSeederTable extends Seeder
     {
         //
         \DB::table('admins')->delete();
-        
+
 
         DB::table('admins')->insert([
             'name' => 'Adebayo',
@@ -45,68 +45,6 @@ class UserSeederTable extends Seeder
 
 
 
-        \DB::table('merchants')->delete();
-        \DB::table('investment_products')->delete();
-
-        DB::table('merchants')->insert([
-            'name' => 'Lotus Halal Capital',
-            'code' => 'lothal',
-            'email' => 'hello@lotus.com',
-            'state' => 'Lagos',
-            'country' => 'Nigeria',
-            'created_at' => Carbon::parse(now())->format('Y.m.d'),
-        ]);
-
-        DB::table('investment_products')->insert([
-            'merchant_id' => 1,
-            'name' => 'Umaiza Short Term',
-            'code' => 'umzshort',
-            'minimum_capital' => 10000,
-            'maximum_capital' => 50000,
-            'duration' => 30,
-            'forecasted_profit' => 7,
-            'created_at' => Carbon::parse(now())->format('Y.m.d'),
-        ]);
-
-        DB::table('investment_products')->insert([
-            'merchant_id' => 1,
-            'name' => 'Umaiza Mid Term',
-            'code' => 'umzmid',
-            'minimum_capital' => 10000,
-            'maximum_capital' => 100000,
-            'duration' => 60,
-            'forecasted_profit' => 7.5,
-            'created_at' => Carbon::parse(now())->format('Y.m.d'),
-        ]);
-
-        DB::table('investment_products')->insert([
-            'merchant_id' => 1,
-            'name' => 'Umaiza Long Term',
-            'code' => 'umzlong',
-            'minimum_capital' => 10000,
-            'maximum_capital' => 150000,
-            'duration' => 39,
-            'forecasted_profit' => 8,
-            'created_at' => Carbon::parse(now())->format('Y.m.d'),
-        ]);
-
-
-        DB::table('merchants')->insert([
-            'name' => 'Sterling Alternative',
-            'code' => 'stalt',
-            'email' => 'hello@sterling.com',
-            'state' => 'Lagos',
-            'country' => 'Nigeria',
-            'created_at' => Carbon::parse(now())->format('Y.m.d'),
-        ]);
-        DB::table('merchants')->insert([
-            'name' => 'ARM Ethical Investments',
-            'code' => 'armlnv',
-            'email' => 'hello@arm.com',
-            'state' => 'Lagos',
-            'country' => 'Nigeria',
-            'created_at' => Carbon::parse(now())->format('Y.m.d'),
-        ]);
 
 
     }
